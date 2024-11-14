f0049991c72e17d2b1e3edaaffff75e4c7a8bf04
Aronpinkman <00149320@uca.edu.sv>
2024-11-14 17:59:17 -0600

    add:Footer

--- SeeToEat-Client/SeeToEat/src/components/RestaurantCard.jsx
+++ SeeToEat-Client/SeeToEat/src/components/RestaurantCard.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "../styles/RestaurantCard.css";
 
 const RestaurantCard = ({ name, image }) => {
-  return (
-    <div className="restaurant-card">
-      <img src={image} alt={name} className="restaurant-image" />
-      <p className="restaurant-name">{name}</p>
-    </div>
-  );
-};
+    return (
+      <div className="restaurant-card">
+        <div className="restaurant-image-container">
+          <img src={image} alt={name} className="restaurant-image" />
+        </div>
+        <p className="restaurant-name">{name}</p>
+      </div>
+    );
+  };
+  
 
 export default RestaurantCard;
