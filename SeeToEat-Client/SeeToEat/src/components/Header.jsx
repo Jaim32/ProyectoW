9121ebb9cc7acdf20dd7950b4ba2ec59f645b01c
Aronpinkman <00149320@uca.edu.sv>
2024-11-14 15:16:01 -0600

    add:routeREstaurantOnHeader

--- SeeToEat-Client/SeeToEat/src/components/Header.jsx
+++ SeeToEat-Client/SeeToEat/src/components/Header.jsx
@@ -13,12 +13,15 @@ function Header() {
   const handleRegister = () => {
     navigate('/welcome'); // Redirige a la página de bienvenida
   };
+  const handleRestaurants = () =>{
+    navigate('/restaurant');
+  }
 
   return (
     <header className="header">
       <h1 className="logo">SeeToEat</h1>
       <nav className="nav">
-        <a href="#restaurantes">Restaurantes</a>
+        <a  onClick={handleRestaurants} href="#restaurantes">Restaurantes</a>
         <a href="#explorar">Explorar</a>
         <button onClick={handleLogin} className="landing-btn login-btn">Iniciar Sesión</button>
         <button onClick={handleRegister} className="landing-btn register-btn">Registrarme</button>
