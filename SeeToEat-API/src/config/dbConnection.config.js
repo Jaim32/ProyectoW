817c24a94527301a5b988bf166bb78bdb7ff0621
Aronpinkman <00149320@uca.edu.sv>
2024-11-15 02:28:07 -0600

    add:Backend

---
+++ SeeToEat-API/src/config/dbConnection.config.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+export const connectiondb = async()=>{
+ const URI = process.env.MONGODB_URI;
+
+ if(!URI){
+
+ throw new Error('No se ha definido la URI de la base')
+ }
+ try{
+ await mongoose.connect(URI);
+ console.log('Base de datos conectada');
+ }catch(e){
+ console.error(e);
+ throw new Error('Error a la hora de iniciar la base de datos')
+ }
+}
