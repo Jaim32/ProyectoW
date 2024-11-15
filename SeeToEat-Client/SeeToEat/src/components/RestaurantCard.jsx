bec5356666c31f26374093016dad8a772c89faff
Aronpinkman <00149320@uca.edu.sv>
2024-11-14 19:33:42 -0600

    add:RestaurantPage ajustado

--- SeeToEat-Client/SeeToEat/src/components/RestaurantCard.jsx
+++ SeeToEat-Client/SeeToEat/src/components/RestaurantCard.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import "../styles/RestaurantCard.css";
 
 const RestaurantCard = ({ name, image }) => {
-    return (
-      <div className="restaurant-card">
-        <div className="restaurant-image-container">
-          <img src={image} alt={name} className="restaurant-image" />
-        </div>
-        <p className="restaurant-name">{name}</p>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="restaurant-card">
+      <img src={image} alt={name} className="restaurant-image" />
+      <p className="restaurant-name">{name}</p>
+    </div>
+  );
+};
 
 export default RestaurantCard;
