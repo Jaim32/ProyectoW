40c43a6c9fa4590a2927d0760d5754e82223e692
Jaim32 <00071322@uca.edu.sv>
2024-11-14 23:44:29 -0600

    hidden

--- SeeToEat-Client/SeeToEat/src/components/Header.jsx
+++ SeeToEat-Client/SeeToEat/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../styles/Header.css';
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Lista de rutas en las que no se deben mostrar los botones de "Iniciar Sesión" y "Registrarme"
+  const routesWithoutButtons = ['/welcome', '/login', '/restaurant', '/restaurantDetail','/register','/localR','/user', '/UserProfile'];
+
+  // Verifica si la ruta actual está en la lista de rutas sin botones
+  const hideButtons = routesWithoutButtons.includes(location.pathname);
 
   const handleLogin = () => {
     navigate('/welcome'); // Redirige a la página de bienvenida
@@ -13,17 +19,22 @@ function Header() {
   const handleRegister = () => {
     navigate('/welcome'); // Redirige a la página de bienvenida
   };
-  const handleRestaurants = () =>{
+
+  const handleRestaurants = () => {
     navigate('/restaurant');
-  }
+  };
 
   return (
     <header className="header">
       <h1 className="logo">SeeToEat</h1>
       <nav className="nav">
-        <a  onClick={handleRestaurants} href="#restaurantes">Restaurantes</a>
-        <button onClick={handleLogin} className="landing-btn login-btn">Iniciar Sesión</button>
-        <button onClick={handleRegister} className="landing-btn register-btn">Registrarme</button>
+        <a onClick={handleRestaurants} href="#restaurantes">Restaurantes</a>
+        {!hideButtons && (
+          <>
+            <button onClick={handleLogin} className="landing-btn login-btn">Iniciar Sesión</button>
+            <button onClick={handleRegister} className="landing-btn register-btn">Registrarme</button>
+          </>
+        )}
       </nav>
     </header>
   );
